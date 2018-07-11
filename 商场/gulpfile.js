51ba3d186258eec3d346d5e5de82f9a3776bfa04
lanbaokun <lanbaokun_l@163.com>
2018-07-11 19:40:16 +0800

    头脚完成

---
+++ 商场/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var server = require('gulp-webserver');
+var path = require('path');
+var url = require('url');
+var fs = require('fs')
+gulp.task('server', function() {
+    gulp.src('src')
+        .pipe(server({
+            port: 8888,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                console.log(pathname)
+                if (req.url === 'favicon.ico') {
+                    return false
+                }
+                pathname = pathname === '/' ? '/index.html' : pathname;
+                res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+            }
+        }))
+})
